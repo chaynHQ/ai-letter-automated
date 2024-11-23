f26b60df5cbf38198588a0d552aa247731529486
yublee71 <lybin71@naver.com>
2024-11-23 15:10:56 +0000

    style(nav): add responsive text size

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -11,12 +11,12 @@ export function Navigation() {
             <Link href="/" className="flex items-center h-[32px] w-[32px]">
               <Image src={logo} alt="logo"/>
             </Link>
-            <Link href="/about" className="text-gray-700 hover:text-gray-900">
+            <Link href="/about" className="text-gray-700 hover:text-gray-900 text-xs sm:text-base">
               About Chayn
             </Link>
             <Link
               href="/get-involved"
-              className="text-gray-700 hover:text-gray-900"
+              className="text-gray-700 hover:text-gray-900 text-xs sm:text-base"
             >
               Get involved
             </Link>
@@ -24,14 +24,14 @@ export function Navigation() {
             <div className="flex items-center space-x-8">
             <Link
               href="/resources"
-              className="text-gray-700 hover:text-gray-900"
+              className="text-gray-700 hover:text-gray-900 text-xs sm:text-base"
             >
               All resources
             </Link>
-            <Link href="/help" className="text-gray-700 hover:text-gray-900">
+            <Link href="/help" className="text-gray-700 hover:text-gray-900 text-xs sm:text-base">
               Get help
             </Link>
-            <Link href="/relax" className="text-gray-700 hover:text-gray-900">
+            <Link href="/relax" className="text-gray-700 hover:text-gray-900 text-xs sm:text-base">
               Relax
             </Link>
           </div>

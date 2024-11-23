dae0c26858edd732f589957686768a13a510297b
yublee71 <lybin71@naver.com>
2024-11-23 15:39:41 +0000

    style(nav): changing font-size and gap for mobile screen

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,8 +6,8 @@ export function Navigation() {
   return (
     <nav className="bg-peach-150 shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-          <div className="flex items-center space-x-8">
+        <div className="flex justify-between h-16 gap-1">
+          <div className="flex items-center space-x-6">
             <Link href="/" className="flex items-center h-[32px] w-[32px]">
               <Image src={logo} alt="logo"/>
             </Link>
@@ -21,7 +21,7 @@ export function Navigation() {
               Get involved
             </Link>
             </div>
-            <div className="flex items-center space-x-8">
+            <div className="flex items-center space-x-6">
             <Link
               href="/resources"
               className="text-gray-700 hover:text-gray-900 text-xs sm:text-base"
